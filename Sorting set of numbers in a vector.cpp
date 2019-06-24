8d473a3d147b90d71bea54ba1852a595044d5212
Mustafa Al-Elq <43064479+mstfelq@users.noreply.github.com>
2019-06-24 09:20:06 +0300

    Update Sorting set of numbers in a vector.cpp

--- Sorting set of numbers in a vector.cpp
+++ Sorting set of numbers in a vector.cpp
@@ -7,18 +7,14 @@
 using namespace std;
 
 int main() {
-
   vector<int> number;
   string s;
- 
-    while (cin >> s, s != "done") {     // While the inputted string is not "done" do...
-      stringstream ss(s);               //
+    while (cin >> s, true) {
+    if (!(s[0] >= '0' && s[0] <= '9')) {break;} // If the string isn't a number break the loop.
+      stringstream ss(s);               
       int x;                            //  Define a stringstream, and some integer x,
       ss >> x;                          //  convert the string into integer x
       number.push_back(x);              //  Add x to vector "number"
   }
   return 0;
 }
-
-
-

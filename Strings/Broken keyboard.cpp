735d860f5930d45679408169cc98a6d8c9e29fa0
Mustafa Al-Elq <43064479+mstfelq@users.noreply.github.com>
2019-06-24 17:12:51 +0300

    Update Broken keyboard.cpp

--- Strings/Broken keyboard.cpp
+++ Strings/Broken keyboard.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+
 using namespace std;
 
 // A solution to https://codeforces.com/problemset/problem/1185/B
 
 
 void insert(string str, vector<pair<char, int>> &a) {             // a function converts a string to pairs of chars with # of appearances
-  int n = str.size();
-  for (int i = 0; i < n - 1; i++) {
+
+  for (int i = 0; i < str.size();) {
       int count = 1;
-      for (int j = 1; str[i] == str[i+j]; j++) count++;            // counting frequency of a character
+      for (int j = 1; i+j < str.size() && str[i] == str[i+j]; j++) count++;            // counting frequency of a character
       a.push_back({str[i], count});                                // pushing it into a pair to the vector
+      i += count;
     }
 }
 
@@ -20,13 +22,12 @@ int main() {
   vector<pair<char,int>> y;
   string str1, str2;
   cin >> str1 >> str2;
-  int n = x.size();
   insert(str1, x);
   insert(str2, y);
-  bool flag = 1;
-
-  for (int i = 0; i < n && flag; i++)
+  bool flag = x.size() == y.size();
+  for (int i = 0; i < x.size() && flag; i++)
       flag &= (x[i].first == y[i].first)&&(x[i].second <= y[i].second);
-  flag?cout<<"YES":cout<<"NO";
+      if (flag) cout << "YES";
+      else cout << "NO";
   return 0;
 }

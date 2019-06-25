3bc896504d30a2837213ffb5d01dc68fc51d0e46
Mustafa Al-Elq <43064479+mstfelq@users.noreply.github.com>
2019-06-25 11:33:08 +0300

    Update isPrime.cpp

--- Number Theory/isPrime.cpp
+++ Number Theory/isPrime.cpp
@@ -1,14 +1,1 @@
-#include <iostream>
-#include <sstream>
-#include <math.h>
-using namespace std;
 
-int main() {
-   for (int p = 2; p < 101; p++) {
-     cout << p << " - ";
-     bool flag = 1;
-    for (int i = 2; i*i <= p; i++) flag &= (p % i != 0);
-    cout << (flag? "prime\n" : "composite\n");
-    }
-  return 0;
-}

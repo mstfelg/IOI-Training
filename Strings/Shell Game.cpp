524dd32c7dad2b1e575f3d0759aee4b9eef1d423
Mustafa Al-Elq <43064479+mstfelq@users.noreply.github.com>
2019-06-25 14:57:33 +0300

    Update Shell Game.cpp

--- Strings/Shell Game.cpp
+++ Strings/Shell Game.cpp
@@ -1,6 +1,10 @@
-// Shell game
-// A solution to https://open.kattis.com/problems/trik
-
+    /*
+    Title           :   Shell Game
+    Author          :   ALELQ, MUSTAFA
+    Date            :   25 June, 2019
+    Description     :   Solution to: https://open.kattis.com/problems/trik
+                        See elso   : https://en.wikipedia.org/wiki/Shell_game
+    */
 
 #include <iostream>
 #include <sstream>
@@ -16,16 +20,18 @@ void swap(int &a, int &b) {
 int main() {
   string str;
   cin >> str;
-  int p1 = 1, p2 = 0, p3 = 0;        // declaring initial positions
+  
+  // declaring initial positions
+  int p1 = 1, p2 = 0, p3 = 0;        
 
   for (int i = 0; i < str.size(); i++) {
       if      (str[i] == 'A')   swap(p1, p2);
       else if (str[i] == 'B')   swap(p2, p3);
       else                      swap(p3, p1);
     }
-
+  // determining position
   if      (p1 == 1) cout << "1";
-  else if (p2 == 1) cout << "2";     // determining position 
+  else if (p2 == 1) cout << "2";
   else              cout << "3";
 
   return 0;

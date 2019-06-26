8a71c1bd4328b9be2b2f09685c62f10eb44aba6b
Mustafa Al-Elq <43064479+mstfelq@users.noreply.github.com>
2019-06-26 10:44:34 +0300

    Update Boy or Girl.cpp

--- Strings/Boy or Girl.cpp
+++ Strings/Boy or Girl.cpp
@@ -3,6 +3,7 @@
     Author          :   ALELQ, MUSTAFA
     Date            :   26 June, 2019
     Description     :   Solution to: https://codeforces.com/problemset/problem/236/A
+                        Determining the parity of distinct characters in a string
     */
 
 #include <bits/stdc++.h>
@@ -11,17 +12,19 @@ using namespace std;
 // Check if charactar ch is in string st
 bool inStr(char ch, string str) {
     bool flag = false;
+    // Pass through each str[i], update flag when you find str[i] == ch & stop the loop.
     for (int i = 0; !flag && i < str.size(); i++) flag |= (str[i]==ch);
     return flag;
 }
 
 int main() {
+    // Define strTemp to assemble distinct letters from str.
     string str, strTemp;
     cin >> str;
     strTemp = "";
     // For each char in str, if it's NOT in strTemp then add it:
     for (int i = 0; i < str.size(); i++) if(!inStr(str[i], strTemp)) strTemp += str[i];
-    // Check parity
+    // Check parity & print the result
     cout << ((strTemp.size() % 2 == 0) ? ("CHAT WITH HER!") : ("IGNORE HIM!"));
     return 0;
 }

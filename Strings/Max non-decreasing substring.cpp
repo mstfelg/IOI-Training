d871b94885748cb3aa3adc93e12e2c863107dcc5
Mustafa Al-Elq <43064479+mstfelq@users.noreply.github.com>
2019-06-26 14:42:31 +0300

    Update Max non-decreasing substring.cpp

--- Strings/Max non-decreasing substring.cpp
+++ Strings/Max non-decreasing substring.cpp
@@ -9,31 +9,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 
-int nonDec(vector<int> a, int i) {
-    int count = 1;
-    // If i is NOT in the range of indeces return 0.
-    if (!(i <= a.size()-1 && i >= 0)) return 0;
-    // Otherwise check each two consecutive indices
-    else {
-        for (int j = 1; ( a[i+j-1] <= a[i+j] ) && ( i+j < a.size() ) ; j++) count++;
-    }
-    return count;
-}
-
 int main() {
-    int max = 1, n;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >>  n;
     // Vector of numbers
     vector<int> nums;
-    cin >> n;
     // Adding all numbers into nums
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; ++i) {
         int k;
         cin >> k;
         nums.push_back(k);
     }
-    // Finding maximum among nonDec
-    for (int i = 0; i < n; i++) if (max < nonDec(nums, i)) max = nonDec(nums, i);
+    // counting non-decreasing length
+    int max = 0;
+    for (int i = 0; i < n; ++i) {
+        int count = 1;
+        for (int j = 1; ( nums[i+j-1] <= nums[i+j] ) && i+j < nums.size() ; j++) {count++;}
+        if (max < count) max = count;
+    }
     cout << max;
     return 0;
 }

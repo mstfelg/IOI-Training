ee92e4d5990d35dbdc50de7ea18dae2cb0731c18
Mustafa Al-Elq <43064479+mstfelq@users.noreply.github.com>
2019-06-27 18:41:06 +0300

    Create Flipped01.cpp

---
+++ Strings/Flipped01.cpp
@@ -0,0 +1,54 @@
+    /*
+    Title           :   Flipping game
+    Author          :   ALELQ, MUSTAFA
+    Date            :   23 June, 2019
+    Description     :   Solution to: https://codeforces.com/problemset/problem/327/A
+                        Given a binary string (of 1's & 0's) where to you flip consecutive digits once so that
+                        you get maximum # of 1?
+    */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    // # of (0's + 1's)
+    int n;
+    cin >> n;
+    // Insert into an array & count 1's, 0's
+    int count0 = 0, count1 = 0;
+    int v[n];
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+        // At the same time count # of 1's & # of 0's
+        if (v[i] == 0) {
+            count0++;
+        }
+        else {
+            count1++;
+        }
+    }
+    int max1 = 0;
+    // Choose a start
+    for (int i = 0; i < n; i++) {
+      // Choose an end
+        for (int j = i; j < n; j++) {
+            // Count # of flipped 1's & 0's
+            int tempCount0 = 0, tempCount1 = 0;
+            // Start from i to j pass through each digit
+            for (int k = 0; i+k <= j; k++) {
+            // Flip the counting
+            if      (v[i+k] == 0)  tempCount1++;
+            else                   tempCount0++;
+            }
+            // Maximize the total number of 1's
+            max1 = max(max1, count1 + tempCount1 - tempCount0);
+        }
+    }
+    // Output
+    cout << max1;
+    return 0;
+}
